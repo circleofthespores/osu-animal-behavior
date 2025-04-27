d32ab4b134101b71dc9fffbaebedeca377e27eb6
Samantha Gustafson <gustasam@oregonstate.edu>
2025-04-27 00:08:27 -0700

    Added top cited table (used in the google doc). Updated code to export this data.

--- final_research/author_clusters.R
+++ final_research/author_clusters.R
@@ -1,7 +1,7 @@
 library(bibliometrix)
 library(knitr)
 
-
+# Load the BibTex file into a dataframe
 author_data <- convert2df(
     file = "data/author_records.bib",
     dbsource = "wos",
@@ -9,6 +9,7 @@ author_data <- convert2df(
     remove.duplicates = T
 )
 
+# create the collaboration network
 collab_network <- biblioNetwork(
     author_data,
     analysis = "collaboration",
@@ -16,6 +17,7 @@ collab_network <- biblioNetwork(
     sep = ";"
 )
 
+# plot the collaboration network so we can visualize the clusters
 collab_network_plt <- networkPlot(
     collab_network,
     n = 30,
@@ -26,3 +28,10 @@ collab_network_plt <- networkPlot(
     labelsize = 0.7,
     edgesize = 5
 )
+
+# Create a table of the top 10 most cited authors
+top_cited <- author_data[
+    order(-author_data$TC), c("AU", "DT", "TC"),
+][1:10, ]
+# export it so we can grab the data
+write.csv(top_cited, "data/top_cited.csv", row.names = FALSE)
